c04a66c34eb483265b7b727dc416d8f68fd143b9
Avily Silva <avilyrs@gmail.com>
2023-06-10 20:53:05 -0300

    :feat: scroll force

--- assets/scripts/carouselControls.js
+++ assets/scripts/carouselControls.js
@@ -1,18 +1,21 @@
+const scrollForceNext = 200;
+const scrollForcePrev = -200;
+
 const experiencesCarousel = document.querySelectorAll('[role=experiencesCarousel]')[0];
 const projectsCarousel = document.querySelectorAll('[role=projectsCarousel]')[0];
 
 function experiencesCarouselScrollToLeft() {
-  experiencesCarousel.scrollBy(-200, 0);
+  experiencesCarousel.scrollBy(scrollForcePrev, 0);
 }
 
 function experiencesCarouselScrollToRight() {
-  experiencesCarousel.scrollBy(200, 0);
+  experiencesCarousel.scrollBy(scrollForceNext, 0);
 }
 
 function projectsCarouselScrollToLeft() {
-  projectsCarousel.scrollBy(-100, 0);
+  projectsCarousel.scrollBy(scrollForcePrev, 0);
 }
 
 function projectsCarouselScrollToRight() {
-  projectsCarousel.scrollBy(100, 0);
+  projectsCarousel.scrollBy(scrollForceNext, 0);
 }
