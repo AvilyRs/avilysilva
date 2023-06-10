7bc4fdeaa18e84770e4a92060e7b5d00242bb3ee
Avily Silva <avilyrs@gmail.com>
2023-06-10 20:38:00 -0300

    :feat: projects section

--- assets/scripts/carouselControls.js
+++ assets/scripts/carouselControls.js
@@ -1,4 +1,5 @@
 const experiencesCarousel = document.querySelectorAll('[role=experiencesCarousel]')[0];
+const projectsCarousel = document.querySelectorAll('[role=projectsCarousel]')[0];
 
 function experiencesCarouselScrollToLeft() {
   experiencesCarousel.scrollBy(-200, 0);
@@ -6,4 +7,12 @@ function experiencesCarouselScrollToLeft() {
 
 function experiencesCarouselScrollToRight() {
   experiencesCarousel.scrollBy(200, 0);
+}
+
+function projectsCarouselScrollToLeft() {
+  projectsCarousel.scrollBy(-100, 0);
+}
+
+function projectsCarouselScrollToRight() {
+  projectsCarousel.scrollBy(100, 0);
 }
